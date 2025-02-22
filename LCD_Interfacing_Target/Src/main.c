24cf6405d42b094ab5c291fa8242b2f0dc87a3a4
Adithya Sharma Mukurala <adithyasharma1703@gmail.com>
2025-02-22 14:48:31 +0530

    made changes to uart_target and added new project lcd interfacing with stm32

---
+++ LCD_Interfacing_Target/Src/main.c
@@ -0,0 +1,13 @@
+
+
+#include <stdint.h>
+#include "stm32f411xe.h"
+
+
+
+int main(void)
+{
+    /* Loop forever */
+	for(;;);
+}
+
--- UART_Target/Src/uart.h
+++ UART_Target/Src/uart.h
@@ -11,14 +11,16 @@
 #define GPIOAEN (1U<<0)
 //PA2 - TX, PA3 - RX
 #define UART2EN (1U<<17)
-
+#define CR1_TE (1U<<3) //8
+#define CR1_UE (1U<<13)
+#define SR_TXE (1U<<7)
 #define SYS_FREQ 16000000 //16 MHz common for nucleo board
 #define APB1_CLK SYS_FREQ
 
-#define UART_BAUDRATE 115200
+#define UART_BAUDRATE 9600
 
 void uart2_tx_init(void);
-uint16_t compute_USART_BRR(uint32_t PeripheralCLK, uint32_t Baudrate);
+uint32_t compute_USART_BRR(uint32_t PeripheralCLK, uint32_t Baudrate);
 void uart2_write(int ch);
 
 
